383d68eb15aee410f3768b905b13ffa21249fff4
Alexis Cureno <alexis.cureno@gmail.com>
2022-07-04 14:33:49 -0500

    changing a little thing

--- color-ext.py
+++ color-ext.py
@@ -1,6 +1,6 @@
 import extcolors
 import PIL
 
-img = PIL.Image.open('../Clock.jpg')
-colors, pixel_count = extcolors.extract_from_image(img)
+image = PIL.Image.open('../Clock.jpg')
+colors, pixel_count = extcolors.extract_from_image(image)
 print(colors)
